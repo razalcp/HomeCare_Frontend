d3ddcbf8ddda92a87a1029f68bdb567b48f8d88d
razalcp <cprazalnazim@gmail.com>
2025-06-21 12:21:28 +0530

    env_edited_versel

--- src/components/DoctorComponents/DoctorHome.tsx
+++ src/components/DoctorComponents/DoctorHome.tsx
@@ -100,7 +100,7 @@ export default function DoctorDashboard({ }: DoctorDashboardProps) {
         return (
             <div className="min-h-screen bg-gray-50 p-6 flex items-center justify-center">
                 <div className="text-center">
-                    <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin mx-auto mb-4"></div>
+                    <div className="w-16 h-16 border-4 border-blue-400 border-t-transparent rounded-full animate-spin mx-auto mb-4"></div>
                     <p className="text-gray-600">Loading dashboard data...</p>
                 </div>
             </div>
@@ -386,7 +386,7 @@ export default function DoctorDashboard({ }: DoctorDashboardProps) {
                     </Card>
                 </div>
 
-                
+
             </div>
         </div>
     )
