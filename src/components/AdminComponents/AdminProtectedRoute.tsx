f36cb68adf11e2ba0d75e6d29b943cc3bc501b4b
razalcp <cprazalnazim@gmail.com>
2025-06-23 12:54:31 +0530

    admin_protected_route_issue_fix

--- src/components/AdminComponents/AdminProtectedRoute.tsx
+++ src/components/AdminComponents/AdminProtectedRoute.tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 
 const AdminProtectedRoute = () => {
   const adminInfo = useSelector((state: any) => state.admin?.adminInfo); // Ensure correct path
+  console.log("This is adminInfo", adminInfo);
 
   return adminInfo ? <Outlet /> : <Navigate to="/adminLogin" />;
 };
