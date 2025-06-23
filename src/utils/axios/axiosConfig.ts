5486e3fed6d98238bed582b3e320e273373da7c2
razalcp <cprazalnazim@gmail.com>
2025-06-23 13:00:15 +0530

    interceptor_fix

--- src/utils/axios/axiosConfig.ts
+++ src/utils/axios/axiosConfig.ts
@@ -15,45 +15,45 @@ const adminApi = axios.create({
     withCredentials: true
 })
 
-const getTokenByRole = (instance: any) => {
-    if (instance === userApi) return localStorage.getItem('userToken');
-    if (instance === doctorApi) return localStorage.getItem('doctorToken');
-    if (instance === adminApi) return localStorage.getItem('adminToken');
-    return null;
-};
+// const getTokenByRole = (instance: any) => {
+//     if (instance === userApi) return localStorage.getItem('userToken');
+//     if (instance === doctorApi) return localStorage.getItem('doctorToken');
+//     if (instance === adminApi) return localStorage.getItem('adminToken');
+//     return null;
+// };
 
 
-const addRequestInterceptor = (apiInstance: any) => {
-    apiInstance.interceptors.request.use(
-        (config: any) => {
-            const token = getTokenByRole(apiInstance);
-            if (token) {
-                config.headers.Authorization = `Bearer ${token}`;
-            }
-            return config;
-        },
-        (error: any) => Promise.reject(error)
-    );
-};
+// const addRequestInterceptor = (apiInstance: any) => {
+//     apiInstance.interceptors.request.use(
+//         (config: any) => {
+//             const token = getTokenByRole(apiInstance);
+//             if (token) {
+//                 config.headers.Authorization = `Bearer ${token}`;
+//             }
+//             return config;
+//         },
+//         (error: any) => Promise.reject(error)
+//     );
+// };
 
 
-const addResponseInterceptor = (apiInstance: any) => {
-    apiInstance.interceptors.response.use(
-        (response: any) => response,
-        (error: any) => {
-            if (error.response?.status === 401) {
-                console.warn("Unauthorized - maybe redirect to login?");
-            }
-            return Promise.reject(error);
-        }
-    );
-};
+// const addResponseInterceptor = (apiInstance: any) => {
+//     apiInstance.interceptors.response.use(
+//         (response: any) => response,
+//         (error: any) => {
+//             if (error.response?.status === 401) {
+//                 console.warn("Unauthorized - maybe redirect to login?");
+//             }
+//             return Promise.reject(error);
+//         }
+//     );
+// };
 
 
-[userApi, doctorApi, adminApi].forEach(api => {
-    addRequestInterceptor(api);
-    addResponseInterceptor(api);
-});
+// [userApi, doctorApi, adminApi].forEach(api => {
+//     addRequestInterceptor(api);
+//     addResponseInterceptor(api);
+// });
 
 export { doctorApi, adminApi }
 
